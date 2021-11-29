c42d54b53b6ab4bba65b37bbf972d3e74f007d19
Tomasz Wiklo <wikweb@gmail.com>
2021-11-29 14:06:05 +0100

    add empty reducer

---
+++ learn-redux/client/components/PhotoGrid.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class PhotoGrid extends Component {
+    render() {
+        return (
+            <div>
+                PhotoGrid
+            </div>
+        )
+    }
+}
+
+export default PhotoGrid
---
+++ learn-redux/client/components/Single.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Single extends Component {
+    render() {
+        return (
+            <div>
+                Single
+            </div>
+        )
+    }
+}
+
+export default Single
