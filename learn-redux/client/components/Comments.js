7d439f424ccd9501a7c34666eb4fe66c0c90a5dc
Tomasz Wiklo <wikweb@gmail.com>
2021-12-02 15:05:14 +0100

    add comment remove

--- learn-redux/client/components/Comments.js
+++ learn-redux/client/components/Comments.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react'
 export class Comments extends Component {
     constructor(props) {
         super(props);
+
+        this.postId = this.props.params.postId;
         this.submitHandler = this.submitHandler.bind(this);
+        this.removeCommentHandler = this.removeCommentHandler.bind(this);
     }
 
     renderComments(comments) {
@@ -13,21 +16,23 @@ export class Comments extends Component {
                     <p>
                         <strong>{comment.user}</strong>
                         {comment.text}
-                        <button className="remove-comment">&times;</button>
+                        <button onClick={this.removeCommentHandler.bind(null, index)} className="remove-comment">&times;</button>
                     </p>
                 </div>
             )
         })
     }
 
+    removeCommentHandler(index) {
+        this.props.removeComment(this.postId, index);
+    }
+
     submitHandler(event) {
         event.preventDefault();
-        const { postId } = this.props.params;
         const author = this.refs.author.value;
         const comment = this.refs.comment.value;
-        
         if (author && comment) {
-            this.props.addComment(postId, author, comment);
+            this.props.addComment(this.postId, author, comment);
             this.refs.commentForm.reset();
             this.refs.author.focus();
         }
--- learn-redux/client/reducers/comments.js
+++ learn-redux/client/reducers/comments.js
@@ -1,15 +1,29 @@
-function comments(state = [], action) {
+function postComments(state = [], action) {
+    let newState = [...state];
     switch(action.type) {
         case "ADD_COMMENT":
-            let newState = {...state};
-            newState[action.postId].push({
+            newState.push({
                 text: action.comment,
                 user: action.author
             });
             return newState;
+        case "REMOVE_COMMENT":
+            const { index } = action;
+            newState.splice(index, 1);
+            return newState;
         default:
             return state;
     }
+}
+
+function comments(state = [], action) {
+    if (typeof action.postId !== 'undefined') {
+        return {
+            ...state,
+            [action.postId]: postComments(state[action.postId], action)
+        }
+    };
+    return state;
 }
 
 export default comments;
